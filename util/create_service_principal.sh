d7a6b5d602325994e0c781e1573c885247407fba
Ben Moss <bmoss@centiq.co.uk>
2020-02-18 21:10:58 +0000

    Add script to generate SP auth details and prevent them being committed to the repo

---
+++ util/create_service_principal.sh
@@ -0,0 +1,122 @@
+#!/usr/bin/env bash
+
+# exit immediately if a command fails
+# set -o errexit
+
+# exit immediately if an unsed variable is used
+set -o nounset
+
+
+# name of the script where the auth info will be saved
+readonly auth_script='set-sp.sh'
+
+
+function main()
+{
+	check_command_line_arguments "$@"
+
+	local service_principal_name="$1"
+
+	create_service_principal_script "${service_principal_name}"
+}
+
+
+function check_command_line_arguments()
+{
+	# Check there's just a single argument provided
+	case $# in
+		1)
+			:
+			;;
+		*)
+			continue_or_error_and_exit 1 "You must specify a single command line argument for the service principal name"
+			;;
+	esac
+}
+
+
+function create_service_principal_script()
+{
+	local service_principal_name="$1"
+
+	check_auth_script_does_not_exist
+
+	echo "Creating Azure Service Principal: ${service_principal_name}..."
+
+	# ensure newlines in variable values are preserved
+	# backup current value to restore to afterwards
+	local ifs_backup="${IFS}"
+	IFS=$'\n'
+
+	# create the service principal and capture the output
+	sp_details=$(az ad sp create-for-rbac --name "${service_principal_name}")
+
+	# determine authorization credentials
+	local subscription_id
+	local tenant_id
+	local client_id
+	local client_secret
+	subscription_id=$(az account show --query 'id')
+	tenant_id=$(az account show --query 'tenantId')
+	client_id=$(echo "${sp_details}"  | grep appId | sed -e 's/.*appId.:.\(.*\),/\1/')
+	client_secret=$(echo "${sp_details}" | grep password | sed -e 's/.*password.:.\(.*\),/\1/')
+
+	# create new bash script for authorization
+	create_new_auth_script
+	append_line_to_auth_script "#!/usr/bin/env bash"
+	append_line_to_auth_script "export ARM_SUBSCRIPTION_ID=${subscription_id}"
+	append_line_to_auth_script "export ARM_TENANT_ID=${tenant_id}"
+	append_line_to_auth_script "export ARM_CLIENT_ID=${client_id}"
+	append_line_to_auth_script "export ARM_CLIENT_SECRET=${client_secret}"
+
+	# restore to previous value
+	IFS="${ifs_backup}"
+
+	echo "A service principal has been created in Azure > App registrations, with the name: ${service_principal_name}"
+	echo "Azure authorization details can be found within the script: ${auth_script}"
+	echo "You can enable this authorization by sourcing the script using the following command:"
+	echo "source ${auth_script}"
+}
+
+
+function check_auth_script_does_not_exist()
+{
+	[ ! -f ${auth_script} ]
+	auth_exists=$?
+	continue_or_error_and_exit $auth_exists "Authorization file already exists: ${auth_script}. Please reuse, move, or remove it."
+}
+
+
+function create_new_auth_script()
+{
+	# create a new empty file that can be executed
+	true > ${auth_script}
+	chmod 0755 ${auth_script}
+}
+
+
+function append_line_to_auth_script()
+{
+	local auth_script_line="$1"
+
+	# append line to file
+	echo "${auth_script_line}" >> ${auth_script}
+}
+
+
+# Given a return/exit status code (numeric argument)
+#   and an error message (string argument)
+# This function returns immediately if the status code is zero.
+# Otherwise it prints the error message to STDOUT and exits.
+# Note: The error is prefixed with "ERROR: " and is sent to STDOUT, not STDERR
+function continue_or_error_and_exit()
+{
+	local status_code=$1
+	local error_message="$2"
+
+	((status_code != 0)) && { echo "ERROR: ${error_message}"; exit 1; }
+}
+
+
+# Execute the main program flow with all arguments
+main "$@"
