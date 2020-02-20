b427b7e6dbef540a4eb45ebf33ea964685d03ad7
Ben Moss <bmoss@centiq.co.uk>
2020-02-20 12:18:33 +0000

    Fix semantic version regex for Ubuntu

--- util/check_workstation.sh
+++ util/check_workstation.sh
@@ -44,7 +44,7 @@ function display_tool_version()
 
 	# The following filtering command should extract the tool name and a 3-part semantic version
 	# Note: Mac/OSX uses BSD sed by default that does not support [0-9]+ so use [0-9][0-9]* 
-	local filter_cmd="sed -e 's/^\([A-z0-9-]*\).*\([0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\).*/\1 = \2/' -e 'tx' -e 'd' -e ':x'"
+	local filter_cmd="sed -e 's/^\([A-Za-z0-9-]*\).*\([0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\).*/\1 = \2/' -e 'tx' -e 'd' -e ':x'"
 
 	local tool_output
 	# Get first line of version output and truncate spaces
