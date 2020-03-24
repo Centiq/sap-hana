10e8b581b1a1799df815d5e673656aaa80ce5201
Ben Moss <bmoss@centiq.co.uk>
2020-03-24 23:19:08 +0000

    Refactor utils script for common JSON edits

--- util/set_resource_group.sh
+++ util/set_resource_group.sh
@@ -10,19 +10,13 @@
 ###############################################################################
 
 # exit immediately if a command fails
-# set -o errexit
+set -o errexit
 
 # exit immediately if an unset variable is used
 set -o nounset
 
 # import common functions that are reused across scripts
 source util/common_utils.sh
-
-
-# location of the input JSON template
-readonly target_path="deploy/v2"
-# readonly target_code="${target_path}/terraform/"
-readonly target_template_dir="${target_path}/template_samples"
 
 
 function main()
@@ -51,52 +45,20 @@ function check_command_line_arguments()
 
 function list_available_templates()
 {
-	print_allowed_json_template_names "${target_template_dir}" | grep -v 'rti_only'
-}
-
-
-function check_command_installed()
-{
-	local cmd="$1"
-	local advice="$2"
-
-	local is_cmd_installed
-	command -v "${cmd}" > /dev/null
-	is_cmd_installed=$?
-
-	local error="This script depends on the '${cmd}' command being installed"
-	# append advice if any was provided
-	if [[ "${advice}" != "" ]]; then
-		error="${error} (${advice})"
-	fi
-
-	continue_or_error_and_exit ${is_cmd_installed} "${error}"
+	print_allowed_json_template_names "${target_template_dir}"
 }
 
 
 function edit_json_template_for_resource_group()
 {
-	local resource_group="$1"
-	local template_name="$2"
+	local rg_name="$1"
+	local json_template_name="$2"
+	local rg_name_json_path='"infrastructure", "resource_group", "name"'
 
-	# use temp file method to avoid BSD sed issues on Mac/OSX
-	# See: https://stackoverflow.com/questions/5694228/sed-in-place-flag-that-works-both-on-mac-bsd-and-linux/5694430#5694430
-	local target_json="${target_template_dir}/${template_name}.json"
-	local temp_template_json="${target_json}.tmp"
-
-	check_file_exists "${target_json}"
-
-	check_command_installed 'jq' 'Try: https://stedolan.github.io/jq/download/'
-
-	# this is the JSON path in jq format
-	local jq_json_path='"infrastructure", "resource_group", "name"'
-	local jq_command="jq --arg rg ${resource_group} 'setpath([${jq_json_path}]; \$rg)' ${target_json}"
-
-	# edit JSON template file contents and write to temp file
-	eval "${jq_command}" > "${temp_template_json}"
-
-    # replace original JSON template file with temporary edited one
-    mv "${temp_template_json}" "${target_json}"
+	# Only attempt to set for non-empty resource groups
+	if [[ "${rg_name}" != "" ]]; then
+		edit_json_template_for_path "${rg_name_json_path}" "${rg_name}" "${json_template_name}"
+	fi
 }
 
 
