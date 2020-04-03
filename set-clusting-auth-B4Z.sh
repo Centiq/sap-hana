fb8e33ba35c1bba94e32c3a2b65d1968907746a0
Mark Murphy <mmurphy@centiq.co.uk>
2020-04-03 14:42:31 +0000

    file removal

--- set-clusting-auth-B4Z.sh
+++
@@ -1,4 +0,0 @@
-export ARM_SUBSCRIPTION_ID="0cbd16ac-c0b4-4c4a-9bca-4ca95477f0a9"
-export ARM_TENANT_ID="0df5a3ce-fce9-426d-b4d3-1603f3deed39"
-export ARM_CLIENT_ID="7639c7bd-3c6d-4fcf-8c32-c4c1bc0fcd6e"
-export ARM_CLIENT_SECRET="463f1797-e801-4090-839f-c9aeaf44cbd4"
