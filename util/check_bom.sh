5c541412ca88bc137c1fb6f38d86231ec3269a96
Rob Fallows <rob.fallows@gmail.com>
2020-12-10 11:58:07 +0000

    filter output

--- util/check_bom.sh
+++ util/check_bom.sh
@@ -3,10 +3,12 @@
 # Usage
 #   /path/to/util/check_bom.sh /other/path/to/bom.yml
 
-command -v yamllint >/dev/null || sudo apt install -y yamllint
-command -v ansible-lint >/dev/null || sudo apt install -y ansible-lint
+YAML_LINT=$(command -v yamllint) || echo "yamllint not found. Try sudo apt install -y yamllint and try again"
+ANSIBLE_LINT=$(command -v ansible-lint) || echo "ansible-lint not found. Try sudo apt install -y ansible-lint and try again"
 
-yamllint $1 && echo "... yamllint [ok]" || echo "... yamllint [errors]"
-ansible-lint $1 && echo "... ansible-lint [ok]" || echo "... ansible-lint [errors]"
+[[ -n ${YAML_LINT} ]] && ${YAML_LINT} $1 && echo "... yamllint [ok]" || echo "... yamllint [errors]"
+[[ -n ${ANSIBLE_LINT} ]] && ${ANSIBLE_LINT} $1 && echo "... ansible-lint [ok]" || echo "... ansible-lint [errors]"
 
-ansible-playbook --extra-vars "bom_name=$1" check_bom.yml
+ansible-playbook --extra-vars "bom_name=$1" check_bom.yml |
+  sed -n -e '/^failed/,/^}/p' |
+  sed -n -e '/"msg":/s/^.*"msg": "\(.*\)".*$/- \1/p'
